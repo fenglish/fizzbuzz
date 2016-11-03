69c7f23710995e9dec5c5f77f458bf9413c55634
Asuka Ochi <asukaochi@gmail.com>
2016-11-03 14:17:54 +0000

    code REFACTOR

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(number)
-    'fizz'
+    if    number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
+    elsif number % 3 == 0 then 'fizz'
+    elsif number % 5 == 0 then 'buzz'
+    else  number
+    end
 end
