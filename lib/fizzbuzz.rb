450aa47161757e7bc85c2f22729fbf3b17019cc7
Asuka Ochi <asukaochi@gmail.com>
2016-11-10 09:46:20 +0000

    test

--- lib/fizzbuzz.rb
+++
@@ -1,7 +0,0 @@
-def fizzbuzz(number)
-    if    number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
-    elsif number % 3 == 0 then 'fizz'
-    elsif number % 5 == 0 then 'buzz'
-    else  number
-    end
-end
